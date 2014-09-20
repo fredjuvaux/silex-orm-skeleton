95cb99a2b9b3ccce14a596a6c51fb37e987ccd4c
Fred J <fredjuvaux@users.noreply.github.com>
2014-09-20 18:01:08 +0200

    Update prod.php

--- config/prod.php
+++ config/prod.php
@@ -2,5 +2,5 @@
 
 // configure your app for the production environment
 
-$app['twig.path'] = array(__DIR__.'/../templates');
+$app['twig.path'] = array(__DIR__.'/../src/views');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
